cbe58a335d07c1c7daa9ede9278f89194f302cd1
loigithubb <117846560+loigithubb@users.noreply.github.com>
2024-11-03 21:55:19 +0700

    Lauch Screen.png

--- BaiTapLon/App.js
+++ BaiTapLon/App.js
@@ -14,13 +14,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Screen4"
+        initialRouteName="Screen1"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Screen1" component={Screen1} />
-        <Stack.Screen name="Screen2" component={Screen2} />
+        {/* <Stack.Screen name="Screen2" component={Screen2} />
         <Stack.Screen name="Screen3" component={Screen3} />
-        <Stack.Screen name="Screen4" component={Screen4} />
+        <Stack.Screen name="Screen4" component={Screen4} /> */}
 
       </Stack.Navigator>
     </NavigationContainer>
