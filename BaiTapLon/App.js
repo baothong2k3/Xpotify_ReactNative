a31ded85672c7ef368c1b21d929305e15e5bad23
MSI\PC BAO THONG <baothong15082003@gmail.com>
2024-11-11 17:08:34 +0700

    Update Lauch Screen

--- BaiTapLon/App.js
+++ BaiTapLon/App.js
@@ -22,7 +22,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="FeedScreen"
+        initialRouteName="Screen1"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Screen1" component={Screen1} />
--- BaiTapLon/Screen/Screen1.js
+++ BaiTapLon/Screen/Screen1.js
@@ -2,75 +2,88 @@ import React from "react";
 import {
   View,
   Text,
-  Button,
   Image,
   TouchableOpacity,
-  TextInput,
   ScrollView,
+  StyleSheet,
+  Dimensions
 } from "react-native";
-
+const screenWidth = Dimensions.get('window').width;
 const Screen1 = ({ navigation }) => {
   return (
-    <ScrollView>
-      <View style={{ width: "100%", height: "100%" }}>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Image
+        resizeMode="cover"
+        source={require("../assets/1_image/Image 30.png")}
+        style={styles.backgroundImage}
+      />
+      <View style={styles.overlay}>
         <Image
-          resizeMode="cover"
-          source={require("../assets/1_image/Image 30.png")}
-        />
-        <Image
-          style={{ position: "absolute", top: 50, right: 130 }}
+          style={styles.logo}
           source={require("../assets/1_image/Image33.png")}
         />
         <Image
-          style={{ position: "absolute", top: 200, right: 50 }}
+          style={styles.logo}
           source={require("../assets/1_image/Your musicYour artists.png")}
         />
-        <TouchableOpacity
-          onPress={() => navigation.navigate("Screen2")}
-          style={{
-            position: "absolute",
-            top: 450,
-            left: 10,
-            backgroundColor: "black",
-            width: 340,
-            height: 50,
-            borderRadius: 10,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <View style={{}}>
-            <Text style={{ color: "white", fontSize: 20 }}>
-              Create an account
-            </Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => navigation.navigate("Screen2")}
-          style={{
-            position: "absolute",
-            top: 530,
-            left: 10,
-            backgroundColor: "rgb(235, 253, 255)",
-            width: 340,
-            height: 50,
-            borderRadius: 10,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <TouchableOpacity onPress={() => navigation.navigate("Screen2")}>
-          <View style={{}}>
-            <Text style={{ color: "rgb(77, 188, 200)", fontSize: 20 }}>
-              I already have an account
-            </Text>
-          </View>
+        <View>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Screen2")}
+            style={[styles.button, styles.createButton]}
+          >
+            <Text style={styles.createButtonText}>Create an account</Text>
           </TouchableOpacity>
 
-        </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Screen2")}
+            style={[styles.button, styles.loginButton]}
+          >
+            <Text style={styles.loginButtonText}>I already have an account</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </ScrollView>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  overlay: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  logo: {
+    marginVertical: '15%'
+  },
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+  },
+  button: {
+    width: screenWidth * 0.9,
+    height: 60,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  createButton: {
+    backgroundColor: "black"
+  },
+  createButtonText: {
+    color: "white",
+    fontSize: 25,
+  },
+  loginButton: {
+    backgroundColor: "rgb(235, 253, 255)",
+  },
+  loginButtonText: {
+    color: "rgb(77, 188, 200)",
+    fontSize: 25,
+  },
+});
 
 export default Screen1;
